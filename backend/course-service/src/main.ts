ff2890ff650202de301f82a10626f034d6deeaf8
Sachin Lakshan <SachinL@fortude.co>
2025-01-28 10:52:17 +0530

    port change for course microservice

--- backend/course-service/src/main.ts
+++ backend/course-service/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001, () => {
+  await app.listen(3001, () => {
     console.log(`Server ready at http://localhost:3001`);
   });
 }
