f99297ef999111fd73f677b1971e639fd859f92f
Sachin Lakshan <SachinL@fortude.co>
2025-01-29 18:26:33 +0530

    install kendo ui grid

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KENDO_BUTTONS } from '@progress/kendo-angular-buttons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KENDO_BUTTONS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  public onButtonClick(): void {
+    console.log('click');
+    alert('Kendo Angular UI Button was clicked.');
+  }
+}
