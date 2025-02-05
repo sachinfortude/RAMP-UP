b86e1bbd7c36906916a137b45d3ee22e14876a76
Sachin Lakshan <SachinL@fortude.co>
2025-02-05 14:05:38 +0530

    remove redundant field age from student entity

--- backend/students-service/src/student/entities/student.entity.ts
+++ backend/students-service/src/student/entities/student.entity.ts
@@ -45,19 +45,4 @@ export class Student {
 
   @Field(() => Course)
   course: Course;
-
-  // Virtual column to calculate age
-  get age(): number {
-    const today = new Date();
-    const birthDate = new Date(this.dateOfBirth);
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDifference = today.getMonth() - birthDate.getMonth();
-    if (
-      monthDifference < 0 ||
-      (monthDifference === 0 && today.getDate() < birthDate.getDate())
-    ) {
-      age--;
-    }
-    return age;
-  }
 }
