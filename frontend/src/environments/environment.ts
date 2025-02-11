d46561113fb10e72269df7eb20a520bd542867f5
Sachin Lakshan <SachinL@fortude.co>
2025-02-11 21:12:45 +0530

    refactor: use environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:4000/graphql',
   fileUploadUrl: 'http://localhost:3000',
-  courseId: '1c3fceff-8b5e-4ed3-afd7-1bc35d09ebac',
+  courseId: 'cd841225-4d8c-456e-8f5c-c84d4ac61f91',
 };
