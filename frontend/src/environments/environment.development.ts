a9b56d3f3db2b7a589fa4003d7e35a24f3a55804
Sachin Lakshan <SachinL@fortude.co>
2025-02-12 18:30:10 +0530

    refactor: env variables

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000/graphql',
   fileUploadUrl: 'http://localhost:3000',
-  courseId: '1c3fceff-8b5e-4ed3-afd7-1bc35d09ebac',
+  courseId: '38d71b24-e1a1-43f2-87c3-be2cdda6bb76',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:4000/graphql',
   fileUploadUrl: 'http://localhost:3000',
-  courseId: 'cd841225-4d8c-456e-8f5c-c84d4ac61f91',
+  courseId: 'f248011e-7380-4ba8-b595-11d31fbdfe54',
 };
